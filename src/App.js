6713dff5e25ab870c7dd6ff1f0dccc9c09f02d96
Neil Chauhan <neilchauhan2@gmail.com>
2019-09-05 22:19:49 +0530

    basic setup for subdomain routing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CreateDomain from "./components/domain/CreateDomain";
 import './App.css';
 
+// extract current doman/subdomain
+const host = window.location.hostname;
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route exact path="/createDomain" component={CreateDomain} />
+          { //routes for localhost or the base url 
+            host === "localhost" ?
+              <Route exact path="/createDomain" component={CreateDomain} />
+              : // routes for the subdomain 
+              <h1> 404 page does not exist! </h1>}
         </Switch>
       </div>
     </Router>
